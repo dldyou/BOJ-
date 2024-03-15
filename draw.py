d4852cb4365f98cedbdef09f512dd01ebf4aa9ba
dldyou <60469410+dldyou@users.noreply.github.com>
2024-03-15 20:27:01 +0900

    edit: unify divisions

--- draw.py
+++ draw.py
@@ -3,55 +3,50 @@ import hashlib
 
 # 추첨 인원수
 winner_num = 5
-# 문제 수
-problems_num = 5
 # BOJ 연습란을 텍스트로 긁어오면 됩니다 (랭킹, 아이디, A, B, C, ... 맨 윗줄 제외하고)
 info = """
 
 """
+info = info.splitlines(keepends = True)
+if info[0] == "\n": info.pop(0)
+
 # 랜덤 시드
 mod = 4294967296 # 2^32
 seed_string = "Hello, AlKon!"
 random_seed = int.from_bytes(hashlib.sha256(seed_string.encode()).digest(), 'big') % mod
 np.random.seed(random_seed)
 
-# 정보 처리
-delimiter = (5 + problems_num * 3)
-info = info.split()
-participants_num = len(info) // delimiter # 참가자 수
-participants_list = [''] * participants_num # 참가자 명단
-correct_problems = [0] * participants_num # 각 참가자가 푼 문제 개수
-
-for i in range(participants_num):
-    participants_list[i] = info[i * delimiter + 1]
-    for j in range(problems_num):
-        if info[i * delimiter + 1 + j * 3] != '--':
-            correct_problems[i] += 1
-
+# 참가자가 각 div에서 푼 최대 문제수 (통합)
+participants = {}
+for participant in info:
+    user = participant.split()[1]
+    corrects = participant.count('/') - participant.count('--') - 1
+    if user in participants:
+        participants[user] = max(participants[user], corrects)
+    else: participants[user] = corrects
+    
 # 추첨 명단 제외 리스트 
 except_list = []
 for except_user in except_list:
     try:
-        idx = participants_list.index(except_user)
-        participants_list.pop(idx)
-        correct_problems.pop(idx)
-        participants_num -= 1
+        participants.pop(except_user)
     except:
         pass
     
 # 추첨 확률 설정
-winner_percent = [0] * participants_num
-correct_problems_sum = sum(correct_problems)
+winner_percent = [0] * len(participants)
+correct_problems_sum = sum(participants.values())
 
-for i in range(participants_num):
-    winner_percent[i] = correct_problems[i] / correct_problems_sum
+for i, corrects in enumerate(list(participants.values())):
+    winner_percent[i] = corrects / correct_problems_sum
 
+# 정보 출력
 print(f'랜덤 시드: {seed_string}')
-print(f'참가자: {participants_list}')
-print(f'맞은 문제 개수: {correct_problems}')
+print(f'참가자: {list(participants.keys())}')
+print(f'맞은 문제 개수: {list(participants.values())}')
 print(f'확률: {winner_percent}')
 
 # 당첨자
-winner = np.random.choice(participants_list, winner_num, replace = False, p = winner_percent) \
-    if winner_num < participants_num else participants_list
-print(f'당첨자: {winner}')
+winner = np.random.choice(list(participants.keys()), winner_num, replace = False, p = winner_percent) \
+    if winner_num < len(participants) else list(participants.keys())
+print(f'\n당첨자: {winner}')
